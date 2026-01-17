3c1e085108ed7321edcc42dff8077db06817fa75
apdiggal8449-star <apdiggal8449@gmail.com>
2026-01-17 11:31:02 +0530

    initial update

--- my-project/src/services/api.js
+++ my-project/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-   baseURL: import.meta.env.VITE_URL + '/api'
+   baseURL: "https://task-management-e770.onrender.com/api"
 });
 
 api.interceptors.request.use((req) => {
